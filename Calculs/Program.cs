af3b8a8dcb95f16bc1a6b34aea2017dfd114556a
Aline2 <aline.gringet@laposte.net>
2025-04-08 14:37:10 +0200

    3 retours à la lignes pour tester refus

--- Calculs/Program.cs
+++ Calculs/Program.cs
@@ -22,6 +22,9 @@ namespace Calculs
             val1 = rand.Next(1, 10);
             val2 = rand.Next(1, 10);
             while (choix != "0")
+
+
+
             {
                 // affiche le menu et saisi le choix
                 Console.WriteLine("Addition ....................... 1");
